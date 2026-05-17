805f457de5a114d10de7e917d809d5facebf6d77
wy <525196545@qq.com>
2026-05-17 12:13:03 +0800

    Delete main.py

--- main.py
+++
@@ -1,8 +0,0 @@
-from telegram import Update
-from telegram.ext import ApplicationBuilder, MessageHandler, filters, ContextTypes
-
-from config import BOT_TOKEN, TARGET_STOCKS
-from tools.stock import get_stock
-from agent.analyst import analyze_stock
-
-import asyncio
